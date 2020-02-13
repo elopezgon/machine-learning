0f6afdff9cdfccb1193d36925428ba6634df9dbc
ger <g.duran@me.com>
2020-02-13 11:14:17 -0600

    modify utils: add fancy_dendogram function

--- files/utils.py
+++ files/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
 from matplotlib.colors import ListedColormap
 
 def plot_classf_model(X, y, model, ax, title=None, step=0.01, epsilon=0.1, contour=False):
@@ -10,9 +12,9 @@ def plot_classf_model(X, y, model, ax, title=None, step=0.01, epsilon=0.1, conto
     X: Matrix(mxn, R)
         Matrix of feature vectors with examples
     y: n-dimensional vector 
-        Actual classes for each of the 'm' provided examples.
+        Actual classes for each of the "m" provided examples.
     """
-    custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+    custom_cmap = ListedColormap(["#fafab0","#9898ff","#a0faa0"])
     points_cmap = ListedColormap(["Yellow", "Blue"])
     x0, x1 = X[:,0].min() - epsilon, X[:,0].max() + epsilon
     y0, y1 = X[:,1].min() - epsilon, X[:,1].max() + epsilon
@@ -28,3 +30,32 @@ def plot_classf_model(X, y, model, ax, title=None, step=0.01, epsilon=0.1, conto
     ax.scatter(*X.T, c=y, s=50, edgecolor="k", cmap=points_cmap)
     if title is not None:
         ax.set_title(title)
+
+
+
+def fancy_dendrogram(*args, **kwargs):
+    """
+    Create a truncated dendrogram
+    """
+    max_d = kwargs.pop("max_d", None)
+    if max_d and "color_threshold" not in kwargs:
+        kwargs["color_threshold"] = max_d
+    annotate_above = kwargs.pop("annotate_above", 0)
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get("no_plot", False):
+        plt.title("Hierarchical Clustering Dendrogram (truncated)")
+        plt.xlabel("sample index or (cluster size)")
+        plt.ylabel("distance")
+        for i, d, c in zip(ddata["icoord"], ddata["dcoord"], ddata["color_list"]):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, "o", c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords="offset points",
+                             va="top", ha="center")
+        if max_d:
+            plt.axhline(y=max_d, c="k")
+    return ddata        
